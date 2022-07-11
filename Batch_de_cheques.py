b4cce77eef628bf50f7c6c4449f993e992034481
niko9705 <nicolasgenovale@gmail.com>
2022-07-10 23:33:34 -0300

    Código

---
+++ Batch_de_cheques.py
@@ -0,0 +1,26 @@
+#print(f'El num de cheque es: {}')
+
+nroDeCheque = 6554
+#print(f'El numero de cheque es: {nroDeCheque}')
+CodigoBanco = 864654
+#print(f'El numero de cheque es: {nro')
+CodigoSucursal = 86485
+NumeroCuentaOrigen = 846585168484
+NumeroCuentaDestino = 856363962638
+Valor = 5000
+FechaOrigen = int(input("Ingrese la fecha origen del cheque")) 
+DNI = int(input("Ingrese el DNI"))
+
+FechaPago = 110722
+
+FechaPago = int(input("Ingrese una fecha de pago"))
+
+if FechaPago == 110722:
+    print("El cheque ha sido aprobado y fue emitido")
+    
+elif FechaPago > 110722:
+     print("El cheque no ha sido emitido, por lo tanto el cheque está pendiente pago")
+     
+
+    
+
