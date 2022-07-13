3120e9bbc283486154830cd371d2bade75c0e95d
camilabattistini <camibattistini@gmail.com>
2022-07-13 17:45:04 -0300

    carpeta cheques y archivos

--- Batch_de_cheques/Batch_de_cheques.py
+++ Batch_de_cheques/Batch_de_cheques.py
@@ -5,7 +5,6 @@ CodigoBanco = int(input('Ingrese el código del banco que figura en la parte sup
 CodigoSucursal = int(input("Ingrese el código que figura en la parte inferior del cheque"))
 NumeroCuentaOrigen = int(input("Ingrese el número de cuenta al que pertenezca"))
 NumeroCuentaDestino = int(input("Ingrese el número de la cuenta destinatario "))
-FechaPago = 110722
 Valor = int(input("Ingrese un valor"))
 
 FechaOrigen = int(input("Ingrese la fecha origen del cheque")) 
@@ -14,12 +13,14 @@ DNI = int(input("Ingrese el DNI"))
 
 FechaPago = int(input("Ingrese una fecha de pago"))
 
-if FechaPago == 110722:
-    print("El cheque ha sido aprobado y fue emitido")
-    
-elif FechaPago > 110722:
-     print("El cheque no ha sido emitido, por lo tanto el cheque está pendiente pago")
-     
+FechaFinal = FechaPago + 30 
+FechaDeposito = int(input('Ingrese la fecha de depósito'))
 
-    
+if (FechaPago <= FechaDeposito <= FechaFinal):
+    print("El cheque ha sido emitido y aprobado")
+elif (FechaDeposito < FechaPago):
+    print("El cheque ha sido emitido pero todavía no puede ser depositado")
+else:
+    print("El cheque ya no puede ser depositado") #La fecha de depósito es mayor a la fecha final y por lo tanto se pasaron los 30 días
+
 
